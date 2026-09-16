34a95eacfcc2a739e47f6f9648de969a7e4f8ca8
sakshipandeycse <sakshipandeycse@gmail.com>
2026-09-16 10:35:17 +0530

    Add e-commerce example

---
+++ E-Commerce.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Product
+{
+    int productId;
+    string name;
+    float price;
+
+public:
+    Product(int id, string n, float p)
+    {
+        productId = id;
+        name = n;
+        price = p;
+    }
+
+    Product comparePrice(const Product &p) const
+    {
+        if (price > p.price)
+            return *this;
+        else
+            return p;
+    }
+
+    void display() const
+    {
+        cout << "Product ID: " << productId << endl;
+        cout << "Name: " << name << endl;
+        cout << "Price: " << price << endl;
+    }
+};
+
+int main()
+{
+    Product p1(101, "Laptop", 55000);
+    Product p2(102, "Mobile", 30000);
+
+    Product higher = p1.comparePrice(p2);
+
+    cout << "Product with Higher Price:\n";
+    higher.display();
+
+    return 0;
+}
